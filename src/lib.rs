982123e9c64dab289f01237abc5e3192112fbefe
Nando <nandolawson@gmail.com>
2024-01-15 13:14:27 +0100

    Update lib.rs & main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,11 @@ use rand::Rng;
 pub fn generate_product_key() -> String {
 	let mut rng = rand::thread_rng();
 	loop {
-		// Generate first block of the product key
-		let block_a: String = format!("{:03}", rng
-			.gen_range(000..=998));
+		let block_a: String = format!("{:03}", rng.gen_range(000..=998)); // Generate first block of the product key
 		// Check if it matches all rules
 		if validate_block_a(block_a.clone()) {
 			loop {
-				// Generate second block of the product key
-				let block_b: String = format!("{:03}", rng
-					.gen_range(1111111..=8888888));
+				let block_b: String = format!("{:03}", rng.gen_range(0000000..=8888888)); // Generate second block of the product key
 				// Check if it matches all rules
 				if validate_block_b(block_b.clone()) {
 					// Merge both blocks and send it back to main function
@@ -24,44 +20,37 @@ pub fn generate_product_key() -> String {
 }
 
 pub fn validate_product_key(product_key: String) -> bool {
-	if validate_format(product_key.clone()) == true {
-		// Get first block from product key
-		let block_a: String = product_key[0..=2].to_string();
-		// Get second block from product key
-		let block_b: String = product_key[4..=10].to_string();
-		// Multiple conditions to check
-		return match (
-			validate_block_a(block_a),
-			validate_block_b(block_b)
-		) {
-			(true, true) => true, _ => false,}
-	} else {return false;}
+	match validate_format(product_key.clone()) {
+		true => {
+			let block_a: String = product_key[0..=2].to_string(); // Get first block from product key
+			let block_b: String = product_key[4..=10].to_string(); // Get second block from product key
+			return match (
+				validate_block_a(block_a), // Check if first block is valid
+				validate_block_b(block_b) // Check if second block is valid
+			)
+			{(true, true) => true, _ => false}
+		}
+		false => false
+	}
 }
 
 fn validate_block_a(block_a: String) -> bool {
 	return match (
 		block_a.len() == 3,
-		block_a.parse::<u32>().unwrap() % 111 != 0
-		|| block_a.parse::<u32>().unwrap() / 111 < 3
-		|| block_a.parse::<u32>().unwrap() / 111 > 9,
-	) {(true, true) => true, _ => false,}
+		block_a.parse::<u32>().unwrap() / 111 < 3 || block_a.parse::<u32>().unwrap() / 111 > 9,
+	) {(true, true) => true, _ => false}
 }
 
 fn validate_block_b(block_b: String) -> bool {
 	return match (
 		block_b.to_string().len() == 7,
-		block_b
-			.chars()
-			.filter_map(|c| c.to_digit(10))
-			.sum::<u32>() % 7 == 0,
-	) {(true, true) => true,_ => false,}
+		block_b.chars().filter_map(|c| c.to_digit(10)).sum::<u32>() % 7 == 0,
+	) {(true, true) => true,_ => false}
 }
 
 fn validate_format(product_key: String) -> bool {
 	return match (
 		product_key.len() == 11,
-		product_key
-			.chars()
-			.nth(3) == Some('-'),
-	) {(true, true) => true, _ => false,};
+		product_key.chars().nth(3) == Some('-'),
+	) {(true, true) => true, _ => false}
 }
--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,22 @@ use win95_keytool::validate_product_key;
 
 /*Code*/
 fn main() {
-	// Get arguments from terminal
-	let args: Vec<String> = env::args()
-		.collect();
+	let args: Vec<String> = env::args().collect(); // Get arguments from terminal
 	println!("[Win95 Key Tool]");
 	// Check how many arguments are available
-	if args
-		.len() != 2 {
-		eprintln!("\nAuthor: {}\nVersion: {}\n\nUsage: {} <product key> / generate\n",
-			env!("CARGO_PKG_VERSION"),
-			env!("CARGO_PKG_AUTHORS"),
-			args[0]
-		);
-		std::process::exit(1);
+	match args.len() {
+		2 => (), _ => {eprintln!("\nAuthor: {}\nVersion: {}\n\nUsage: {} <product key> / generate\n",
+				env!("CARGO_PKG_VERSION"),
+				env!("CARGO_PKG_AUTHORS"),
+				args[0]
+			);
+			std::process::exit(1);
+		}
 	}
 	// Load argument into variable and check if it is called "generate"
 	// Everything else will be checked if it is a valid product key
-	let product_key: String = args[1]
-		.to_string();
-	if product_key == "generate" {
-		println!("Windows 95 key: {}", generate_product_key());
-	}
-	else if validate_product_key(product_key.clone()) {
-		println!("Valid key: {}", product_key);
-	}
-	else {
-		println!("Invalid key: {}", product_key);
-	}
+	let product_key: String = args[1].to_string();
+	if product_key == "generate" {println!("Windows 95 key: {}", generate_product_key())}
+	else if validate_product_key(product_key.clone()) {println!("Valid key: {}", product_key)}
+	else {println!("Invalid key: {}", product_key)}
 }
